657aac5de21ba12e35597364b5aaeb6087682646
wentiansky <2087152746@qq.com>
2022-12-05 22:41:45 +0800

    feat: add lazyload2.js

--- 性能优化/加载优化/延迟加载/lazyload2.js
+++ 性能优化/加载优化/延迟加载/lazyload2.js
@@ -2,5 +2,23 @@
 document.addEventListener('DOMContentLoaded', function () {
   const lazyImages = [...document.querySelectorAll('img.lazy')]
   // 判断浏览器兼容性
-  
+  if (
+    'IntersectionObserver' in window &&
+    'IntersectionObserverEntry' in window &&
+    'intersectionRatio' in window.IntersectionObserverEntry.prototype
+  ) {
+    let lazyImageObserver = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          let lazyImage = entry.target
+          lazyImage.src = lazyImage.dataset.src
+          lazyImage.classList.remove('lazy')
+          lazyImageObserver.unobserve(lazyImage)
+        }
+      })
+    })
+    lazyImages.forEach(lazyImage => {
+      lazyImageObserver.observe(lazyImage)
+    })
+  }
 })
