96ef1b8a5bbbb46607c605b562d23e59e607f034
liyin <591459256@qq.com>
2022-12-05 20:37:04 +0800

    feat: add Intersection Observer

--- 性能优化/加载优化/延迟加载/lazyload1.js
+++ 性能优化/加载优化/延迟加载/lazyload1.js
@@ -1,12 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
+// 通过图片上沿离视窗顶部的距离与视窗的高度做判断
+document.addEventListener('DOMContentLoaded', function () {
   let lazyImages = [].slice.call(document.querySelectorAll('img.lazy'))
   // 限制函数被频繁调用
   let active = false
-  const lazyLoad = function() {
+  const lazyLoad = function () {
     if (active === false) {
       active = true
-      setTimeout(function() {
-        lazyImages.forEach(function(lazyImage) {
+      setTimeout(function () {
+        lazyImages.forEach(function (lazyImage) {
           if (
             lazyImage.getBoundingClientRect().top <= window.innerHeight &&
             lazyImage.getBoundingClientRect().bottom >= 0 &&
@@ -14,11 +15,11 @@ document.addEventListener('DOMContentLoaded', function() {
           ) {
             lazyImage.src = lazyImage.dataset.src
             lazyImage.classList.remove('lazy')
-            lazyImages = lazyImages.filters(function(image) {
+            lazyImages = lazyImages.filter(function (image) {
               return image !== lazyImage
             })
             if (lazyImages.length === 0) {
-              document.removeEventListener('scroll', lazyLoad)
+              document.body.removeEventListener('scroll', lazyLoad)
               window.removeEventListener('resize', lazyLoad)
               window.removeEventListener('orientationchange', lazyLoad)
             }
@@ -28,7 +29,8 @@ document.addEventListener('DOMContentLoaded', function() {
       }, 200)
     }
   }
-  document.addEventListener('scroll', lazyLoad)
+  // 需要为图片的容器元素绑定scroll事件，方可生效
+  document.body.addEventListener('scroll', lazyLoad)
   window.addEventListener('resize', lazyLoad)
   window.addEventListener('orientationchange', lazyLoad)
 })
---
+++ 性能优化/加载优化/延迟加载/lazyload2.js
@@ -0,0 +1,6 @@
+// 通过Intersection Observer方式判断
+document.addEventListener('DOMContentLoaded', function () {
+  const lazyImages = [...document.querySelectorAll('img.lazy')]
+  // 判断浏览器兼容性
+  
+})
