9f4543823a7263d053a1b15fc41ce8833455e946
wentiansky <2087152746@qq.com>
2023-03-21 07:56:19 +0800

    feat: update 类型别名

--- TypeScript/进阶/类型别名/type-alias.ts
+++ TypeScript/进阶/类型别名/type-alias.ts
@@ -1,3 +1,4 @@
+// 类型别名常用于联合类型
 type Name = string
 type NameResolver = () => string
 type NameOrResolver = Name | NameResolver
@@ -8,3 +9,6 @@ function getName(name: NameOrResolver): Name {
   }
   return name()
 }
+
+// 声明setTimeout
+type TimeoutHandle = ReturnType<typeof setTimeout>
