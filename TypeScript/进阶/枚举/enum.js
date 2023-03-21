7d35815b704b13392023654e9a6d364678d7c863
wentiansky <2087152746@qq.com>
2023-03-21 08:18:17 +0800

    feat: update enum

--- TypeScript/进阶/枚举/enum.js
+++ TypeScript/进阶/枚举/enum.js
@@ -1,6 +1,8 @@
 // 1. 普通枚举
 // 2. 手动赋值
 // 3. 常数枚举
+// 4. 外部枚举
+// 枚举成员默认从0开始递增，也可以手动赋值
 var Days;
 (function (Days) {
     Days[Days["Sun"] = 7] = "Sun";
@@ -12,4 +14,7 @@ var Days;
     Days[Days["Sat"] = 13] = "Sat";
 })(Days || (Days = {}));
 console.log(Days["Sun"] === 7); // true
+console.log(Days[0] === 'Sun'); // true
 var directions = [0 /* Up */, 1 /* Down */, 2 /* Left */, 3 /* Right */];
+var directions1 = [Directions1.Up, Directions1.Down, Directions1.Left, Directions1.Right];
+console.log(directions1);
--- TypeScript/进阶/枚举/enum.ts
+++ TypeScript/进阶/枚举/enum.ts
@@ -2,10 +2,12 @@
 // 2. 手动赋值
 // 3. 常数枚举
 // 4. 外部枚举
+// 枚举成员默认从0开始递增，也可以手动赋值
 enum Days { Sun = 7, Mon, Tue, Wed, Thu, Fri, Sat }
 console.log(Days["Sun"] === 7); // true
+console.log(Days[0] === 'Sun') // true
 
-// 常数枚举
+// 常数枚举const enum(不能包含计算成员)
 const enum Directions {
   Up,
   Down,
@@ -14,10 +16,12 @@ const enum Directions {
 }
 let directions = [Directions.Up, Directions.Down, Directions.Left, Directions.Right]
 
-// 外部枚举
+// 外部枚举 declare enum
 declare enum Directions1 {
   Up,
   Down,
   Left,
   Right
 }
+let directions1 = [Directions1.Up, Directions1.Down, Directions1.Left, Directions1.Right];
+console.log(directions1)
