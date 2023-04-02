77ee3d5115b5579251110d92af5317f90b944e68
wentiansky <2087152746@qq.com>
2023-04-03 06:51:27 +0800

    feat: add generic

---
+++ TypeScript/进阶/泛型/generic.ts
@@ -0,0 +1,25 @@
+// 1. 概念：泛型指在定义函数、接口和类的时候，不预先指定具体的类型，等到使用的时候再指定类型
+// 2. 多个类型参数
+
+// 1. 概念
+function createArray1(length: number, value: any): Array<any> {
+  let result = []
+  for (let i = 0; i < length; i++) {
+    result[i] = value
+  }
+  return result
+}
+
+createArray1(3, 'x')
+
+function createArray2<T>(length: number, value: T): Array<T> {
+  let result: Array<T> = []
+  for (let i = 0; i < length; i++) {
+    result[i] = value
+  }
+  return result
+}
+
+createArray2<string>(3, 'y')
+
+// 2. 多个类型参数
