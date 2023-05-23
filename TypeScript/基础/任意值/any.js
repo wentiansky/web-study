b082403e73ca98902d178b04251fabacd23fb5d9
wentiansky <2087152746@qq.com>
2023-05-24 07:06:05 +0800

    feat: Add unknown type

--- TypeScript/基础/任意值/any.js
+++ TypeScript/基础/任意值/any.js
@@ -4,4 +4,11 @@ anyVar = 10;
 console.log(anyVar.myName);
 console.log(anyVar.myName.firstName);
 // 声明一个变量为任意值之后，对它的任何操作，返回的内容的类型都是任意值
-// 变量如果在声明的时候，未指定其类型，那么它会被识别为任意值类型
+var str = '{ "name": "jax" }';
+var user = JSON.parse(str);
+var isUser = function (user) {
+    return typeof user.name === 'string';
+};
+if (isUser(user)) {
+    console.log(user.name.length);
+}
--- TypeScript/基础/任意值/any.ts
+++ TypeScript/基础/任意值/any.ts
@@ -5,3 +5,18 @@ anyVar = 10
 console.log(anyVar.myName)
 console.log(anyVar.myName.firstName)
 // 声明一个变量为任意值之后，对它的任何操作，返回的内容的类型都是任意值
+
+const str = '{ "name": "jax" }'
+const user: unknown = JSON.parse(str)
+interface User {
+  name: string
+}
+
+// 类型守卫
+const isUser = (user: any): user is User => {
+  return typeof user.name === 'string'
+}
+
+if (isUser(user)) {
+  console.log(user.name.length)
+}
