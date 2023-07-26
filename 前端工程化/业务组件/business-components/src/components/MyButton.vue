745ede1d9eb9065318f9fc430c016fab99862d31
yin.li <2087152746@qq.com>
2023-07-26 23:06:36 +0800

    feat: add my button component

---
+++ 前端工程化/业务组件/business-components/src/components/MyButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-button v-bind="props">
+    <slot />
+  </el-button>
+</template>
+
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    type?: string
+    [key: string]: any
+  }>(),
+  {
+    type: 'primary'
+  }
+)
+
+// 写一个求和的函数
+</script>
